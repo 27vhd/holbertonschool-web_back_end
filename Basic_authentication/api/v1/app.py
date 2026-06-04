c21d80ecde289415e8d25b320bc1e507fad22727
Vahid Arzumanov <arzumanovvahid27@gmail.com>
2026-06-04 13:59:36 +0400

    Fix E501 line too long in app.py before_request

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -29,7 +29,11 @@ def before_request():
     """ Filter each request """
     if auth is None:
         return
-    excluded = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]    
     if not auth.require_auth(request.path, excluded):
         return
     if auth.authorization_header(request) is None:
