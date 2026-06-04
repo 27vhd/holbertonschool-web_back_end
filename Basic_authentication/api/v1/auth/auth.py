dbaf27a7c8cd94f7ddcf84fe6576f69cf26476ec
Vahid Arzumanov <arzumanovvahid27@gmail.com>
2026-06-04 13:55:01 +0400

    Update require_auth with slash tolerance and exclusion logic

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,13 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require auth """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if not path.endswith('/'):
+            path = path + '/'
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """ Authorization header """
